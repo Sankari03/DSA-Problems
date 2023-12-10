d0cd6f28db4e88983bf3ba5569bd7029ed7a97be
Sankari Prakash <sankariprakash771@gmail.com>
2023-12-10 19:44:15 +0530

    SimpleInterest

---
+++ src/Simple_Programs/SimpleInterest.java
@@ -0,0 +1,22 @@
+//Write a program to input principal, time, and rate (P, T, R) from the user and
+//find Simple Interest.
+
+package Simple_Programs;
+
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Principal Amount - ");
+        double principal = sc.nextDouble();
+        System.out.print("Enter the Number of Years - ");
+        double noOfYears = sc.nextDouble();
+        System.out.print("Enter the Rate of Interest - ");
+        double rateOfInterest = sc.nextDouble();
+
+        double simpleInterest = (principal * noOfYears * rateOfInterest) / 100;
+
+        System.out.println("Simple Interest is - "+simpleInterest);
+    }
+}
