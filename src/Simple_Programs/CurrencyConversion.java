cba8718a25e4890e542056cca06c75f749bac6f3
Sankari Prakash <sankariprakash771@gmail.com>
2023-12-11 13:38:13 +0530

    CurrencyConversion

---
+++ src/Simple_Programs/CurrencyConversion.java
@@ -0,0 +1,17 @@
+//Input currency in rupees and output in USD.
+
+package Simple_Programs;
+
+import java.util.Scanner;
+
+public class CurrencyConversion{
+    public static void main(String[] args) {
+        System.out.println("Enter rupees to convert into USDs : ");
+        Scanner sc = new Scanner(System.in);
+
+        double rupees = sc.nextFloat();
+        double usd = rupees / 83.370153;
+
+        System.out.println(rupees+" rupees is " + usd + " USDs");
+    }
+}
