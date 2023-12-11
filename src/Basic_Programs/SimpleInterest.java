df99df1f24d154fd05b830844759aa5f31eb0b49
Sankari Prakash <sankariprakash771@gmail.com>
2023-12-11 14:19:41 +0530

    SimpleInterest Program

--- src/Basic_Programs/SimpleInterest.java
+++ src/Basic_Programs/SimpleInterest.java
@@ -1,7 +1,7 @@
 //Write a program to input principal, time, and rate (P, T, R) from the user and
 //find Simple Interest.
 
-package Simple_Programs;
+package Basic_Programs;
 
 import java.util.Scanner;
 
