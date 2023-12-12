ba60ef2a2b00479ef46ddf9493d4492519e92cd6
Sankari Prakash <sankariprakash771@gmail.com>
2023-12-12 19:06:26 +0530

    Factors Program

--- src/Basic_Programs/Factors.java
+++ src/Basic_Programs/Factors.java
@@ -11,9 +11,13 @@ public class Factors {
 
         int number = sc.nextInt();
 
-        for (int i = 1; i <= number ; i++) {
-            if (number % i == 0){
-                System.out.print(i + " ");
+        if (number <= 0) {
+            System.out.println("Enter only positive numbers");
+        } else {
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    System.out.print(i + " ");
+                }
             }
         }
     }
