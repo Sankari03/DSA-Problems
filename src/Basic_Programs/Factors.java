882adb27d2b4e4604348c27e533aa2e35ce6a6e6
Sankari Prakash <sankariprakash771@gmail.com>
2023-12-12 13:07:41 +0530

    FactorsOfANumber Program

---
+++ src/Basic_Programs/Factors.java
@@ -0,0 +1,20 @@
+//Input a number and print all the factors of that number (use loops).
+
+package Basic_Programs;
+
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args) {
+        System.out.print("Enter a number to find its factors: ");
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        for (int i = 1; i <= number ; i++) {
+            if (number % i == 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
