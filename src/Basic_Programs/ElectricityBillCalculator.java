99fcdf9efae30c63b8df8cecf7bff266e0ea6776
Sankari Prakash <sankariprakash771@gmail.com>
2023-12-30 15:32:09 +0530

    ElectricityBillCalculator Program

---
+++ src/Basic_Programs/ElectricityBillCalculator.java
@@ -0,0 +1,41 @@
+package Basic_Programs;
+
+import java.util.Scanner;
+
+public class ElectricityBillCalculator {
+    public static void main(String[] args) {
+        System.out.print("Enter the number of units - ");
+        Scanner sc = new Scanner(System.in);
+        double units = sc.nextDouble();
+        double amount = 0;
+        if (units <= 500){
+            if (units <= 100){
+                amount = 0;
+            } else if (units <= 200) {
+                amount = (units-100) * 2.25;
+            }else if (units <= 400) {
+                amount = 100 * 2.25 + (units - 200) * 4.5;
+            }else {
+                amount = 100 * 2.25 + 200 * 4.5 + (units-400) * 6;
+            }
+        } else {
+            if (units <= 100){
+                amount = 0;
+            } else if (units <= 400) {
+                amount = (units-100) * 4.5;
+            }else if (units <= 500) {
+                amount = 400 * 4.5 + (units - 400) *6;
+            }else if (units <= 600) {
+                amount = 400 * 4.5 + 100 * 6 + (units - 500) * 8;
+            }else if (units <= 800) {
+                amount = 400 * 4.5 + 100 * 6 + 100 * 8 + (units - 600) * 9;
+            }else if (units <= 1000) {
+                amount = 400 * 4.5 + 100 * 6 + 100 * 8 + 200 * 9 + (units - 800) * 10;
+            }else if (units > 1000) {
+                amount = 400 * 4.5 + 100 * 6 + 100 * 8 + 200 * 9 + 200 * 10 + (units - 1000) * 11;
+            }
+        }
+
+        System.out.println("Amount to be paid - "+amount);
+    }
+}
