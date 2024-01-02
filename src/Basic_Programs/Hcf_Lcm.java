68a1b47b3671435cc76b1edaffbc675ce2f00b4c
Sankari Prakash <sankariprakash771@gmail.com>
2024-01-02 13:36:14 +0530

    HCF & LCM Program

--- src/Basic_Programs/Hcf_Lcm.java
+++ src/Basic_Programs/Hcf_Lcm.java
@@ -11,15 +11,15 @@ public class Hcf_Lcm {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int gcd = 1;
+        int hcf = 1;
         for (int i = 1; i <= a && i <= b ; i++) {
             if (a % i == 0 && b % i == 0){
-                gcd = i;
+                hcf = i;
             }
         }
-        int lcm = (a*b)/gcd;
+        int lcm = (a*b)/hcf;
 
-        System.out.println("The HCF of the 2 numbers "+a+" "+b+" is "+gcd);
+        System.out.println("The HCF of the 2 numbers "+a+" "+b+" is "+hcf);
         System.out.println("The LCM of the 2 numbers "+a+" "+b+" is "+lcm);
 
     }
